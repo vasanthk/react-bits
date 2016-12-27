02c65b8d1cfba9e31871000943131053148e559e
vasanthk <vasa.httpster@gmail.com>
2016-12-26 19:21:49 -0800

    Controlled input

---
+++ 17.controlled-input.jsx
@@ -0,0 +1,31 @@
+// It’s hard to talk about controlled inputs in the abstract. Let’s start with an uncontrolled (normal) input and go from there.
+
+<input type="text" />
+// When you fiddle with this input in the browser, you see your changes. This is normal.
+
+// A controlled input disallows the DOM mutations that make this possible.
+// You set the value of the input in component-land and it doesn’t change in DOM-land.
+
+<input type="text" value="This won't change. Try it." />
+// Obviously static inputs aren’t very useful to your users. So, we derive a value from state.
+
+class ControlledNameInput extends React.Component {
+  constructor() {
+    super();
+    this.state = {name: ""}
+  }
+
+  render() {
+    return <input type="text" value={this.state.name} />
+  }
+};
+// Then, changing the input is a matter of changing component state.
+return (
+  <input
+    value={this.state.name}
+    onChange={e => this.setState(e.target.value)}
+  />
+);
+
+// This is a controlled input. It only updates the DOM when state has changed in our component. This is invaluable when creating consistent UIs.
+// If you’re using stateless functions for form elements, read about using state hoisting to move new state up the component tree.
