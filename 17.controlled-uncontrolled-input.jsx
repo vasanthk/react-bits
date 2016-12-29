45bfc19eeadb2111db32778ebd2d786973220bba
vasanthk <vasa.httpster@gmail.com>
2016-12-29 14:57:15 -0800

    Uncontrolled output using refs

--- 17.controlled-uncontrolled-input.jsx
+++ 17.controlled-uncontrolled-input.jsx
@@ -8,7 +8,8 @@
  */
 
 // It’s hard to talk about controlled inputs in the abstract. Let’s start with an uncontrolled (normal) input and go from there.
-
+// So, a normal HTML input field effectively stores its own value at all times, and you can get the element and ask for its value.
+// this is what we refer to as an "uncontrolled" input
 <input type="text" />
 // When you fiddle with this input in the browser, you see your changes. This is normal.
 
@@ -35,6 +36,21 @@ return (
     onChange={e => this.setState(e.target.value)}
   />
 );
-
 // This is a controlled input. It only updates the DOM when state has changed in our component. This is invaluable when creating consistent UIs.
 // If you’re using stateless functions for form elements, read about using state hoisting to move new state up the component tree.
+
+// Note:
+// In React, you can still create "uncontrolled" inputs: Using 'refs'
+
+const MyComponent extends React.Component {
+  onClick() {
+    const input = this.refs.myInput;
+    const value = input.value;
+    // do something with the value
+  }
+
+  render() {
+    return <input type="text" ref="myInput" />
+  }
+}
+
