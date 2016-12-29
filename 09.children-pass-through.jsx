a283e032729991e5fc6211ee21d90ac28f4692cb
vasanthk <vasa.httpster@gmail.com>
2016-12-28 23:11:58 -0800

    Children composition

--- 09.children-pass-through.jsx
+++ 09.children-pass-through.jsx
@@ -30,3 +30,33 @@ return React.Children.only(this.props.children);
 // Note: We should mention that there is no API that allow us accessing child's state. Or in other words we can't use this.props.children[0].state or something like that.
 // The proper way of retrieving information from the children is by using props (passing callbacks).
 // And that's a good thing. This approach forces us defining clear APIs and encourage the one-way direction data flow.
+
+// Passing a child as a property
+// Every React component receive props. It's nice that these props may contain all kind of data. Even other components.
+
+// App.jsx
+class App extends React.Component {
+  render() {
+    var title = <h1>Hello there!</h1>;
+
+    return (
+      <Header title={ title }>
+        <Navigation />
+      </Header>
+    );
+  }
+}
+
+// Header.jsx
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        { this.props.title }
+        <hr />
+        { this.props.children }
+      </header>
+    );
+  }
+};
+// This technique is helpful when we have a mix between components that exist inside the Header and components that have to be provided from the outside.
