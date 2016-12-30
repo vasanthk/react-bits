043c44cf925c384f5c092e6ed328de7d48a0d55d
vasanthk <vasa.httpster@gmail.com>
2016-12-30 13:16:04 -0800

    Dependency injection

---
+++ 20.dependency-injection.js
@@ -0,0 +1,15 @@
+/**
+ * Dependency Injection
+ *
+ * @Reference:
+ * What is Dependency Injection?
+ * https://www.youtube.com/watch?v=IKD2-MAkXyQ
+ *
+ * DI in JavaScript:
+ * https://www.youtube.com/watch?v=jXhdOTw1q5Q
+ * http://krasimirtsonev.com/blog/article/Dependency-injection-in-JavaScript
+ *
+ * In React:
+ * https://github.com/krasimir/react-in-patterns/tree/master/patterns/dependency-injection
+ */
+
