d2fdf622fde88ddb704a5713e61520ac6aae112e
vasanthk <vasa.httpster@gmail.com>
2017-01-07 19:23:04 -0800

    Full blown dispatcher

--- 23.flux-pattern.jsx
+++ 23.flux-pattern.jsx
@@ -27,3 +27,58 @@ function register(store) {
     this._stores.push({store: store});
   }
 }
+
+// Full blown Dispatcher
+var Dispatcher = function () {
+  return {
+    _stores: [],
+    register: function (store) {
+      if (!store || !store.update) {
+        throw new Error('You should provide a store that has an `update` method.');
+      } else {
+        var consumers = [];
+        var change = function () {
+          consumers.forEach(function (l) {
+            l(store);
+          });
+        };
+        var subscribe = function (consumer, noInit) {
+          consumers.push(consumer);
+          !noInit ? consumer(store) : null;
+        };
+
+        this._stores.push({store: store, change: change});
+        return subscribe;
+      }
+      return false;
+    },
+    dispatch: function (action) {
+      if (this._stores.length > 0) {
+        this._stores.forEach(function (entry) {
+          entry.store.update(action, entry.change);
+        });
+      }
+    }
+  }
+};
+
+module.exports = {
+  create: function () {
+    var dispatcher = Dispatcher();
+
+    return {
+      createAction: function (type) {
+        if (!type) {
+          throw new Error('Please, provide action\'s type.');
+        } else {
+          return function (payload) {
+            return dispatcher.dispatch({type: type, payload: payload});
+          }
+        }
+      },
+      createSubscriber: function (store) {
+        return dispatcher.register(store);
+      }
+    }
+  }
+};
