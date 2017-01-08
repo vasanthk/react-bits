291d97ce42351b48a8d23d4e94a798ee6c1e5b17
vasanthk <vasa.httpster@gmail.com>
2017-01-07 19:35:15 -0800

    Advantage of using HOC

--- 15.higher-order-component.jsx
+++ 15.higher-order-component.jsx
@@ -44,3 +44,26 @@ const Connect = ComposedComponent =>
 const ConnectedMyComponent = Connect(Greeting);
 // This is a powerful pattern for providing fetching and providing data to any number of stateless function components.
 
+
+// Advantage of using HOC:
+// HOCs give us control on the input data that we want to send as props.
+// Let's say that we have a configuration setting that OriginalComponent needs:
+
+var config = require('path/to/configuration');
+
+var enhanceComponent = (Component) =>
+  class Enhance extends React.Component {
+    render() {
+      return (
+        <Component
+          {...this.state}
+          {...this.props}
+          title={ config.appTitle }
+        />
+      )
+    }
+  };
+
+// The knowledge for the configuration is hidden into the higher-order component.
+// OriginalComponent knows only that it receives a prop called title. Where it comes from it is not important.
+// That's a huge advantage because it helps us testing the component in an isolation and provides nice mechanism for mocking.
