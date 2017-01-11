005fc8ac185a6e63f32158aceb544a71e6a557a6
vasanthk <vasa.httpster@gmail.com>
2017-01-10 20:28:57 -0800

    More ref links

--- presentational-vs-container.jsx
+++ presentational-vs-container.jsx
@@ -4,6 +4,7 @@
  * Reference:
  * https://medium.com/@dan_abramov/smart-and-dumb-components-7ca2f9a7c7d0#.mbglcakmp
  * https://github.com/krasimir/react-in-patterns/tree/master/patterns/presentational-and-container
+ * https://medium.com/@learnreact/container-components-c0e67432e005
  */
 
 // PROBLEM
