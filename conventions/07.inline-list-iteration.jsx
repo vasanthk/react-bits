2c0ce37bfaf27f5537f366d31e3269eb7928b343
vasanthk <vasa.httpster@gmail.com>
2017-01-14 00:05:30 -0800

    Inline list iteration

---
+++ conventions/07.inline-list-iteration.jsx
@@ -0,0 +1,15 @@
+/**
+ * In-line list iteration
+ *
+ * Where possible, iterate over lists of data in-line in the returned JSX unless
+ * its internal logic is sufficiently complex enough to warrant moving outside of
+ * the return statement and populating an array for rendering.
+ */
+
+return (
+  <div>
+    {this.props.list.map(function (data, i) {
+      return (<Component data={data} key={i}/>)
+    })}
+  </div>
+);
