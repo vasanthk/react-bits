9ab5120eefbcb1932ce15e413b382f07ca5bad5e
vasanthk <vasa.httpster@gmail.com>
2017-01-13 23:12:37 -0800

    React class organization

---
+++ conventions/01.class-organization.jsx
@@ -0,0 +1,57 @@
+/**
+ * The class definition for can be organized as follows:
+ *
+ * class definition
+ *    constructor
+ *        event-handlers
+ *    component life-cycle events
+ *    getters
+ *    setters
+ * defaultProps
+ * propTypes
+ *
+ */
+
+class Car extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {running: false};
+
+    this.handleClick = () => {
+      this.setState({running: !this.state.running});
+    };
+  }
+
+  componentWillMount() {
+    // add event listeners (Flux Store, WebSocket, document, etc.)
+  }
+
+  componentDidMount() {
+    // React.findDOMNode()
+  }
+
+  componentWillUnmount() {
+    // remove event listeners (WebSocket, document, etc.)
+  }
+
+  get engineStatus() {
+    return (this.state.running) ? "is running" : "is off";
+  }
+
+  render() {
+    return (
+      <div onClick={this.handleClick}>
+        {this.props.make} {this.engineStatus}
+      </div>
+    );
+  }
+}
+
+Car.defaultProps = {
+  make: 'Toyota'
+};
+
+Car.propTypes = {
+  make: React.PropTypes.string
+};
