deb1783d3d3eca2a02af5dd52c7b6fd5c81dc8bc
vasanthk <vasa.httpster@gmail.com>
2017-03-19 13:31:30 -0700

    propTypes and defaultProps

--- conventions/01.class-organization.jsx
+++ conventions/01.class-organization.jsx
@@ -71,3 +71,34 @@ class ProfileContainer extends Component {
     );
   }
 }
+
+/**
+ * propTypes and defaultProps
+ *
+ * propTypes and defaultProps can be declared as static properties.
+ * They should be immediately visible to other devs reading the file an declared higher up in the file, since they serve as documentation.
+ */
+
+class ProfileContainer extends Component {
+  state = {activeProfile: false};
+
+  static propTypes = {
+    model: React.PropTypes.object.isRequired,
+    title: React.PropTypes.string
+  };
+
+  static defaultProps = {
+    model: {
+      id: 0
+    },
+    title: 'Your Name'
+  };
+
+  render() {
+    return (
+      <div>
+        Is Profile Active: {this.state.activeProfile}
+      </div>
+    );
+  }
+}
