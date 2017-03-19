103fbb6973d8e5bfb571e943c1b036b9cf530563
vasanthk <vasa.httpster@gmail.com>
2017-03-19 16:41:26 -0700

    Related links

--- 28.decorators.jsx
+++ 28.decorators.jsx
@@ -3,6 +3,12 @@
  *
  * If you’re using something like mobx, you can decorate your class components — which is the same as passing the component into a function.
  * Decorators are flexible and readable way of modifying component functionality.
+ *
+ * Related:
+ * https://twitter.com/dan_abramov/status/628202050946514944
+ * https://medium.com/@gigobyte/enhancing-react-components-with-decorators-441320e8606a
+ * http://stackoverflow.com/questions/36553814/what-is-the-use-of-connect-decorator-in-react-redux
+ * http://stackoverflow.com/questions/36286384/decorators-with-react-components
  */
 
 // Non-decorators approach
