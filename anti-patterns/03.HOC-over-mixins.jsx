6f4f263c467733453e3f308b2a5e56b561267288
vasanthk <vasa.httpster@gmail.com>
2017-03-25 14:18:27 -0700

    HOC over mixins

---
+++ anti-patterns/03.HOC-over-mixins.jsx
@@ -0,0 +1,8 @@
+/**
+ * Use Higher order components over Mixins
+ *
+ * @Reference:
+ * https://facebook.github.io/react/blog/2016/07/13/mixins-considered-harmful.html
+ */
+
+// @TODO: Simple code examples here.
