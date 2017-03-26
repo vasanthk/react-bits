387ac879d9b8eadfd773590a45848055d2cf881f
vasanthk <vasa.httpster@gmail.com>
2017-03-26 00:42:38 -0700

    HOC feature toggles

---
+++ ux-variations/03.HOC-feature-toggles.jsx
@@ -0,0 +1,22 @@
+/**
+ * Using Higher order components (HOC) for UX variations
+ *
+ * eg. Toggling features On/Off
+ */
+
+// featureToggle.js
+const isFeatureOn = function (featureName) {
+  // return true or false
+};
+
+import {isDeatureOn} from './featureToggle';
+
+const toggleOn = (featureName, ComposedComponent) => class HOC extends Component {
+  render() {
+    return isFeatureOn(featureName) ? <ComposedComponent {...this.props} /> : null;
+  }
+};
+
+// Usage
+import AdsComponent from './Ads'
+const Ads = toggleOn('ads', AdsComponent);
