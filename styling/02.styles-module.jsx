da36e31401a4182c9d97874a9946c884e059e04d
vasanthk <vasa.httpster@gmail.com>
2017-03-26 00:47:32 -0700

    Styles module

---
+++ styling/02.styles-module.jsx
@@ -0,0 +1,65 @@
+/**
+ * Styles module
+ * Generally, hard coding styles values in a component should be avoided.
+ * Any values that are likely to be used across different UI components should be split into their own module.
+ *
+ * @Reference:
+ * http://jxnblk.com/writing/posts/patterns-for-style-composition-in-react/
+ */
+
+// Styles module
+export const white = '#fff';
+export const black = '#111';
+export const blue = '#07c';
+
+export const colors = {
+  white,
+  black,
+  blue
+};
+
+export const space = [
+  0,
+  8,
+  16,
+  32,
+  64
+];
+
+const styles = {
+  bold: 600,
+  space,
+  colors
+};
+
+export default styles
+
+// Usage
+import React from 'react'
+import { bold, space, colors } from './styles'
+
+const Button = ({
+  ...props
+  }) => {
+  const sx = {
+    fontFamily: 'inherit',
+    fontSize: 'inherit',
+    fontWeight: bold,
+    textDecoration: 'none',
+    display: 'inline-block',
+    margin: 0,
+    paddingTop: space[1],
+    paddingBottom: space[1],
+    paddingLeft: space[2],
+    paddingRight: space[2],
+    border: 0,
+    color: colors.white,
+    backgroundColor: colors.blue,
+    WebkitAppearance: 'none',
+    MozAppearance: 'none'
+  };
+
+  return (
+    <button {...props} style={sx}/>
+  )
+};
